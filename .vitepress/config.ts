85cd9be75540bf0462dcd552e45d3b699ed73b08
Riley Ho <riley@rileychh.dev>
2024-09-10 14:53:58 +0800

    style: lint with ESLint

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vitepress'
-import Components from 'unplugin-vue-components/vite'
-import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
