899b293e286665fdccc77b3fcfa411bfb3828135
Riley Ho <riley@rileychh.dev>
2024-09-16 19:24:15 +0800

    style: lint with ESLint

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,6 @@
+import type { Theme } from 'vitepress'
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme from 'vitepress/theme'
-import type { Theme } from 'vitepress'
 import Layout from './Layout.vue'
 import './style.css'
 
